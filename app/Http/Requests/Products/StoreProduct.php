cac6f736de27ee16d5827e03752015008949cc45
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 15:11:36 -0300

    Request de validação de produtos.

---
+++ app/Http/Requests/Products/StoreProduct.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Products;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProduct extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->user()->isOwner();
+    }
+
+    public function rules()
+    {
+        return [
+            'description' => 'required|min:5|string',
+            'ean' => 'required|size:13|numeric',
+        ];
+    }
+}
