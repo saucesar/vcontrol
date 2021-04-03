bb8ef4d8c76d3730002f30dd9666254c89a83e14
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 14:18:37 -0300

    Exibição de mensagens de status.

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -3,6 +3,10 @@
 @section('content')
     @include('company.header')
     <div class="container-fluid mt--7">
+        <div class="d-flex justify-content-center">
+            @include('components.alerts.error')
+            @include('components.alerts.success')
+        </div>
         <div class="row">
             <div class="col-xl-12 mb-5 mb-xl-0">
                 <div class="row">
