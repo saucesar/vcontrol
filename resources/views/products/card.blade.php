8466033f6daae5eca0f46a266c6bdd4b115913b1
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 02:55:13 -0300

    Ajuste de tamanho do card de produto.

---
+++ resources/views/products/card.blade.php
@@ -0,0 +1,18 @@
+<div class="card card-sized mb-4">
+    <div class="card-body">
+        <div class="row">
+            <div class="col">
+                <h5 class="card-title text-uppercase text-muted mb-0">{{ $product->description }}</h5>
+                <span class="h2 font-weight-bold mb-0">{{ $product->ean }}</span>
+            </div>
+            <div class="col-auto">
+                <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                    <i class="fas fa-box"></i>
+                </div>
+            </div>
+        </div>
+        <p class="mt-3 mb-0 text-muted text-sm">
+            <span class="text-nowrap"></span>
+        </p>
+    </div>
+</div>
