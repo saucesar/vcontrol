8f94be61610c039fac00375f9713e28abb00f7da
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 16:18:32 -0300

    ajuste para edição de produtos.

--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -1,10 +1,10 @@
 <div class="row mb-4">
     <div class="col">
-        @include('components.inputs.text', ['name' => 'description', 'label' => 'Descrição'])
+        @include('components.inputs.text', ['name' => 'description', 'label' => 'Descrição', 'value' => $description ?? ''])
     </div>
 </div>
 <div class="row">
     <div class="col">
-        @include('components.inputs.text', ['name' => 'ean', 'label' => 'Código Barra'])
+        @include('components.inputs.text', ['name' => 'ean', 'label' => 'Código Barra',  'value' => $ean ?? ''])
     </div>
 </div>
