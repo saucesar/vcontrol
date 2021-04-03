3d78f3a877e61a4da2eeaab730b4725361b50cf9
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 14:56:14 -0300

    Form de produtos, para reuso em edição.

---
+++ resources/views/products/form.blade.php
@@ -0,0 +1,10 @@
+<div class="row mb-4">
+    <div class="col">
+        @include('components.inputs.text', ['name' => 'description', 'label' => 'Descrição'])
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        @include('components.inputs.text', ['name' => 'ean', 'label' => 'Código Barra'])
+    </div>
+</div>
