76e21d8518a81c103b7c913dd22a4eb9e26ed83d
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 16:19:04 -0300

    card de status de produto.

---
+++ resources/views/products/stats.blade.php
@@ -0,0 +1,68 @@
+<!-- Card stats -->
+<div class="row mb-4">
+    <div class="col-xl-3 col-lg-6">
+        <div class="card card-stats mb-4 mb-xl-0">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">Traffic</h5>
+                        <span class="h2 font-weight-bold mb-0">350,897</span>
+                    </div>
+                    <div class="col-auto">
+                        <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                            <i class="fas fa-chart-bar"></i>
+                        </div>
+                    </div>
+                </div>
+                <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                    <span class="text-nowrap">Since last month</span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-6">
+        <div class="card card-stats mb-4 mb-xl-0">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+                        <span class="h2 font-weight-bold mb-0">2,356</span>
+                    </div>
+                    <div class="col-auto">
+                        <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                            <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
+                    <span class="text-nowrap">Since last week</span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <h3>Ações</h3>
+            </div>
+            <div class="card_body">
+                <div class="row">
+                    <div class="col">
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalEditProduct{{ $product->id }}" title="Editar um Produto.">
+                            <i class="fas fa-pen"></i>
+                        </button>
+                        @include('products.edit_modal', ['modalId' => "modalEditProduct$product->id", 'product' => $product])
+                    </div>
+                    <div class="col">
+                        <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
