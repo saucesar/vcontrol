5ee2e8b38388d45429c784d556de63ae5f0e403a
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 00:41:50 -0300

    arquivo de traducao de mensagens.

---
+++ resources/lang/pt-br/messages.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'welcome' => 'Bem vindo ao VControl!',
+    'Dashboard' => 'Dashboard',
+    'Register' => 'Registre-se',
+    'Login' => 'Entrar',
+    'Profile' => 'Perfil',
+    'SignIn' => 'Entrar',
+    'SignInWith' => 'Entrar com',
+    'Remember' => 'Lembre de mim',
+    'ForgotPassword' => 'Esqueceu a senha?',
+    'CreateAccount' => 'Criar uma conta.',
+    'SignUpWithCredentials' => 'Cadastrar-se estes dados',
+    'Name' => 'Nome',
+    'Password' => 'Senha',
+    'ConfirmPassword' => 'Confirme a senha',
+    'IAgreeWithThe' => 'Eu concordo com ',
+    'PrivacyPolicy' => 'a Politica de Privacidade',
+    'CreateAccount' => 'Criar conta',
+];
