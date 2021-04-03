24d908aab5e30107bdbb40aa834ae353942de4d4
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 14:29:56 -0300

    Cabeçalho da tela de produtos.

---
+++ resources/views/products/header.blade.php
@@ -0,0 +1,9 @@
+<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="text-center">
+                <h2>Produtos</h2>
+            </div>
+        </div>
+    </div>
+</div>
