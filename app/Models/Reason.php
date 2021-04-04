e3ccda46ef8873ad9b6cdb13ca24873659fc71a3
saucesar <sau.cesarlima2013@gmail.com>
2021-04-04 02:30:05 -0300

    model para justificativa de saída de produto.

---
+++ app/Models/Reason.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Reason extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'company_id'];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id', 'id');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             EmailSeeder::class,
             CategoryEmailSeeder::class,
+            ReasonSeeder::class,
         ]);
     }
 }
