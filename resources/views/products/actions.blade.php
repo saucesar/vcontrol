d7d644b2ee295343d88ca34458f76ebc48767d02
saucesar <sau.cesarlima2013@gmail.com>
2021-04-04 02:11:40 -0300

    Ajuste de estilo dos cards de ação.

--- resources/views/products/actions.blade.php
+++ resources/views/products/actions.blade.php
@@ -1,8 +1,15 @@
 <div class="card">
-    <div class="card-header">
-        <h3>Ações</h3>
-    </div>
-    <div class="card_body">
+    <div class="card-body">
+        <div class="row mb-2">
+            <div class="col">
+                <h5 class="card-title text-uppercase text-muted mb-0">Ações</h5>
+            </div>
+            <div class="col-auto">
+                <div class="icon icon-shape bg-info text-white rounded-circle shadow">
+                    <i class="fas fa-info-circle"></i>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col">
                 <button type="button" class="btn btn-block btn-primary" data-toggle="modal" data-target="#modalEditProduct{{ $product->id }}"
