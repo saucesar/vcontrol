f291397024f53a79aa3a27eddef873799d949dc3
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 21:23:48 -0300

    Listagem de categorias.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request)
+    {
+        !$request->perPage ? : session()->put('categories.perPage', $request->perPage);
+        $perPage = session('categories.perPage', 5);
+        
+        $data = [
+            'categories' => Category::byCompany(auth()->user()->company->id, $perPage),
+        ];
+
+        return view('categories.index', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app', ['active' => 'categories', 'title' => 'Categoria'])
+
+@section('content')
+    @include('categories.header')
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col-xl-12 mb-5 mb-xl-0">
+                @include('categories.stats')
+                <div class="d-flex justify-content-center">
+                    @include('components.alerts.error')
+                    @include('components.alerts.success')
+                </div>
+                <div class="d-flex justify-content-between">
+                    <div>{{ $categories->links() }}</div>
+                    <div>
+                    @if(auth()->user()->isOwner())
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalAddCategory" title="Adicionar uma categoria.">
+                            <i class="ni ni-fat-add"></i>
+                        </button>
+                        @include('categories.add_modal', ['modalId' => 'modalAddCategory'])
+                    @endif
+                    @include('components.per_page', ['route' => route('categories.index'), 'values' => [5, 10, 15, 20]])
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card card-body shadow-lg">
+                            @include('categories.table', ['categories' => $categories])
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    {{ $categories->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
