391630726c63e027aa8bf774944e5bf6a74ee85c
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 22:05:45 -0300

    Adicao de opções de crud de categorias.

---
+++ resources/views/categories/form.blade.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col">
+        @include('components.inputs.text', ['name' => 'name', 'label' => 'Nome', 'value' => $name ?? ''])
+    </div>
+</div>
--- resources/views/categories/table.blade.php
+++ resources/views/categories/table.blade.php
@@ -1,14 +1,36 @@
 <table class="table">
     <thead class="thead-light">
+        <th>ID</th>
         <th>Categoria</th>
-        <th>Produtos</th>
+        <th class="text-center">Produtos</th>
+        <th class="text-center">Ações</th>
     </thead>
     <tbody>
     @foreach($categories as $category)
     <tr>
+        <td>{{ $category->id }}</td>
         <td>{{ $category->name }}</td>
-        <td>{{ $category->products->count() }}</td>
+        <td class="text-center">{{ $category->products->count() }}</td>
+        <td>
+            <div class="d-flex justify-content-center">
+                <a class="btn btn-sm btn-info" href="{{ route('categories.show', $category->id) }}">
+                    <i class="far fa-eye"></i>
+                </a>
+                <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modalEditCategory{{ $category->id }}"
+                        title="Editar um Produto." @if(!auth()->user()->isOwner()) disabled @endif >
+                    <i class="fas fa-pen"></i>
+                </button>
+                <form class="" action="{{ route('categories.destroy', $category->id) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Tem certeza?');">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </form>
+            </div>
+        </td>
     </tr>
+    @include('categories.edit_modal', ['modalId' => "modalEditCategory$category->id", 'category' => $category])
     @endforeach
     </tbody>
 </table>
