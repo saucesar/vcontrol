9be3b6054c4ef559c283daaee634844ddee123fd
saucesar <sau.cesarlima2013@gmail.com>
2021-04-04 03:27:29 -0300

    metodo para data em formato pt-BR.

--- app/Models/Date.php
+++ app/Models/Date.php
@@ -5,12 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
 class Date extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['date', 'amount', 'lote', 'product_id', 'previous_id', 'reason_id'];
+
+    public function date()
+    {
+        return Carbon::parse($this->date)->format('d-m-Y');
+    }
 
     public function product()
     {
