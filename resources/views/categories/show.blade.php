290239be593ee5d373f6bb0e9c8151aa1008096c
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 22:43:20 -0300

    Tela de detalhes de categoria.

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app', ['active' => 'categories', 'title' => 'Categoria'])
+
+@section('content')
+    @include('categories.header')
+    <div class="container-fluid mt--7 card card-body bg-secondary">
+        <div class="row">
+            <div class="col-xl-12 mb-5 mb-xl-0">
+                @include('categories.stats')
+                <div class="d-flex justify-content-center">
+                    @include('components.alerts.error')
+                    @include('components.alerts.success')
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>Lista de Produtos</h3>
+            </div>
+        </div>
+        <div class="d-flex justify-content-between">
+            {{ $products->links() }}
+            <div>
+                @include('components.per_page', ['route' => route('categories.show', $category->id), 'values' => [4, 8, 12, 20]])
+            </div>
+        </div>
+        <div class="card-deck">
+            @foreach($products as $product)
+                @include('products.card', ['product' => $product])
+            @endforeach
+        </div>
+        <div class="d-flex justify-content-start">
+            {{ $products->links() }}
+        </div>
+    </div>
+@endsection
