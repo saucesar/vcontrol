3318dd4739b6b350640613af86d2667cbce2a0c3
saucesar <sau.cesarlima2013@gmail.com>
2021-04-03 21:22:27 -0300

    Cabeçalho de categorias

---
+++ resources/views/categories/header.blade.php
@@ -0,0 +1,10 @@
+<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="text-center">
+                <h2>Categorias</h2>
+                <h3>{{ $category->name ?? '' }}</h3>
+            </div>
+        </div>
+    </div>
+</div>
