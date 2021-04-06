bb59685ff6785c487ca464626c2227339b048bbe
saucesar <sau.cesarlima2013@gmail.com>
2021-04-05 21:42:33 -0300

    modal para exibir gráfico.

---
+++ resources/views/dates/graphic_modal.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="{{ $modalId }}" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Grafico de Evolução</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div id="{{ $chartId }}"></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
