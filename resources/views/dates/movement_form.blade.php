9646e11de68fa340f5649a94f8239869b3058e27
saucesar <sau.cesarlima2013@gmail.com>
2021-04-12 22:55:17 -0300

    Criaçao de modais para adicionar ou diminuir quantidade.

---
+++ resources/views/dates/movement_form.blade.php
@@ -0,0 +1,17 @@
+<div class="row mb-4">
+    <div class="col">
+        @include('components.inputs.number', ['name' => 'amount', 'label' => $amountLabel, 'value' => $amount ?? old('amount') ?? '1'])
+    </div>
+    <div class="col">
+        <label for="reason_id">Motivo</label>
+        <select class="form-control" name="reason_id" id="reason_id" required>
+            <option value="">Selecione</option>
+            @foreach(auth()->user()->company->reasons as $reason)
+            <option value="{{ $reason->id }}" {{ old('reason_id') == $reason->id ? 'selected' : '' }}>{{ $reason->name }}</option>
+            @endforeach
+        </select>
+        @error('reason_id')
+        <small class="text-danger">{{ $message }}</small>
+        @enderror
+    </div>
+</div>
