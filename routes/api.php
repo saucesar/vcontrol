0d116dd3df4414afb63f273f3bf2997802ae2f13
saucesar <sau.cesarlima2013@gmail.com>
2021-06-20 15:25:41 -0300

    remocao de rotas inutilizadas

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
