ce170c22a7f92d2062f98d912ec608a0712668b5
saucesar <sau.cesarlima2013@gmail.com>
2021-08-15 00:26:10 -0300

    input para entradas do tipo float

---
+++ resources/views/components/inputs/float.blade.php
@@ -0,0 +1,16 @@
+<label for="{{ $name }}">{{ $label }}</label>
+@if(isset($prepend))
+<div class="input-group mb-2">
+    <div class="input-group-prepend">
+        <div class="input-group-text"><i class="{{ $prepend }}"></i></div>
+    </div>
+    <input type="number" id="{{ $name }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}"
+           value="{{ $value ?? old($name) }}" {{ isset($readonly) ? 'readonly' : '' }} step="0.01" required>
+</div>
+@else
+<input type="number" id="{{ $name }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}"
+       value="{{ $value ?? old($name) }}" {{ isset($readonly) ? 'readonly' : '' }} step="0.01" required>
+@endif
+@error($name)
+<small class="text-danger">{{ $message }}</small>
+@enderror
