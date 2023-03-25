911ea9ca413083bf2652cb4cae9250f4dba04472
saucesar <sau.cesarlima2013@gmail.com>
2023-03-25 19:14:41 -0300

    remoção de unique em name

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -12,7 +12,7 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->unique()->text(20),
+            'name' => $this->faker->text(30),
             'company_id' => $this->faker->numberBetween(1, \App\Models\Company::count()),
         ];
     }
