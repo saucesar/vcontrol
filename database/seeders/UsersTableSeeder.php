6b99fb27c21bb941c834d1c39d3866652eeebaea
saucesar <sau.cesarlima2013@gmail.com>
2023-03-25 19:14:02 -0300

    Tratamento de exceção.

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 namespace Database\Seeders;
 
+use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,24 +15,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::create([
-            'name' => 'Cesar',
-            'email' => 'cesar@vc.com',
-            'type' =>'owner',
-            'password' => bcrypt('123456'),
-            'company_id' => 1,
-        ]);
+        try {
+            \App\Models\User::create([
+                'name' => 'Cesar',
+                'email' => 'cesar@vc.com',
+                'type' =>'owner',
+                'password' => bcrypt('123456'),
+                'company_id' => 1,
+            ]);
         
-        DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@argon.com',
-            'type' =>'owner',
-            'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+            DB::table('users')->insert([
+                'name' => 'Admin Admin',
+                'email' => 'admin@argon.com',
+                'type' =>'owner',
+                'email_verified_at' => now(),
+                'password' => Hash::make('secret'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        } catch(Exception $e) {}
 
-        \App\Models\User::factory()->times(30)->create();
+        \App\Models\User::factory()->times(10)->create();
     }
 }
