f033b7fc9c0f6d578c41f0a095b63ccef75e61dc
saucesar <sau.cesarlima2013@gmail.com>
2023-04-14 19:41:24 -0300

    Ajuste no input

--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -4,12 +4,15 @@
     <div class="input-group-prepend">
         <div class="input-group-text"><i class="{{ $prepend }}"></i></div>
     </div>
-    <input type="{{ $type ?? 'text' }}" id="{{ $name }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}"
-           value="{{ $value ?? old($name) }}" {{ isset($readonly) ? 'readonly' : '' }} required>
+    <input type="{{ $type ?? 'text' }}" id="{{ $name }}"
+        class="form-control @error($name) is-invalid @enderror {{ $class ?? ''}}" name="{{ $name }}"
+        value="{{ $value ?? old($name) }}" {{ isset($readonly) ? 'readonly' : '' }} required>
 </div>
 @else
-<input type="{{ $type ?? 'text' }}" id="{{ $name }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}"
-       value="{{ $value ?? old($name) }}" {{ isset($readonly) ? 'readonly' : '' }} @if(isset($min)) min="{{ $min }}" @endif required>
+<input type="{{ $type ?? 'text' }}" id="{{ $name }}"
+    class="form-control @error($name) is-invalid @enderror {{ $class ?? ''}}" name="{{ $name }}"
+    value="{{ $value ?? old($name) }}" {{ isset($readonly) ? 'readonly' : '' }} @if(isset($min)) min="{{ $min }}" @endif
+    required>
 @endif
 @error($name)
 <small class="text-danger">{{ $message }}</small>
