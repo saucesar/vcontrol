8bbf8d2630d9193cba4d724962c5d9b35e96bc8a
saucesar <sau.cesarlima2013@gmail.com>
2023-04-14 19:22:45 -0300

    Correção no menu lateral

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,24 +1,27 @@
 <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
     <div class="container-fluid">
         <!-- Toggler -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main"
+            aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <!-- Brand -->
-        <a class="navbar-brand pt-0" href="{{ route('home') }}"><!--
+        <a class="navbar-brand pt-0" href="{{ route('home') }}">
+            <!--
             <img src="{{ asset('argon') }}/img/brand/blue.png" class="navbar-brand-img" alt="...">-->
             <h2 class="text-left text-default">
                 <i class="fas fa-calendar-alt"></i>
-                VControl    
+                VControl
             </h2>
         </a>
         <!-- User -->
         <ul class="nav align-items-center d-md-none">
             <li class="nav-item dropdown">
-                <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
                     <div class="media align-items-center">
                         <span class="avatar avatar-sm rounded-circle">
-                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-1-800x800.jpg">
+                            <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-1-800x800.jpg">
                         </span>
                     </div>
                 </a>
@@ -62,7 +65,9 @@
                         </a>
                     </div>
                     <div class="col-6 collapse-close">
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse"
+                            data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false"
+                            aria-label="Toggle sidenav">
                             <span></span>
                             <span></span>
                         </button>
@@ -72,7 +77,8 @@
             <!-- Form -->
             <form class="mt-4 mb-3 d-md-none">
                 <div class="input-group input-group-rounded input-group-merge">
-                    <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="{{ __('Search') }}" aria-label="Search">
+                    <input type="search" class="form-control form-control-rounded form-control-prepended"
+                        placeholder="{{ __('Search') }}" aria-label="Search">
                     <div class="input-group-prepend">
                         <div class="input-group-text">
                             <span class="fa fa-search"></span>
@@ -88,12 +94,14 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#navbar-account" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-account">
+                    <a class="nav-link" href="#navbar-account" data-toggle="collapse" role="button" aria-expanded="true"
+                        aria-controls="navbar-account">
                         <i class="ni ni-circle-08"></i>
                         <span class="nav-link-text" style="color: #f4645f;">{{ __('Conta') }}</span>
                     </a>
 
-                    <div class="collapse {{ !($active == 'profile') ?: 'show' }} {{ !($active == 'manager')  ?: 'show' }}" id="navbar-account">
+                    <div class="collapse {{ !($active == 'profile') ?: 'show' }} {{ !($active == 'manager')  ?: 'show' }}"
+                        id="navbar-account">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item {{ !($active == 'profile') ? : 'active' }}">
                                 <a class="nav-link" href="{{ route('profile.edit') }}">
@@ -120,20 +128,23 @@
                     </a>
                 </li>
                 <li class="nav-item {{ !($active == 'emails') ? : 'active' }}">
-                    <a class="nav-link" href="{{ route('emails.index') }}">
+                    <a class="nav-link @if(!ExistsCompany::get()) disabled @endif"
+                        href="@if(ExistsCompany::get()) {{ route('emails.index') }} @else # @endif">
                         <i class="ni ni-email-83"></i>
                         {{ __('Emails') }}
                     </a>
                 </li>
                 @endif
                 <li class="nav-item {{ !($active == 'categories') ? : 'active' }}">
-                    <a class="nav-link" href="{{ route('categories.index') }}">
+                    <a class="nav-link @if(!ExistsCompany::get()) disabled @endif"
+                        href="@if(ExistsCompany::get()) {{ route('categories.index') }} @else # @endif">
                         <i class="ni ni-books"></i>
                         {{ __('Categorias') }}
                     </a>
                 </li>
                 <li class="nav-item {{ !($active == 'products') ? : 'active' }}">
-                    <a class="nav-link" href="{{ route('products.index') }}">
+                    <a class="nav-link  @if(!ExistsCompany::get()) disabled @endif"
+                        href="@if(ExistsCompany::get()) {{ route('products.index') }} @else # @endif">
                         <i class="ni ni-app"></i>
                         {{ __('Produtos') }}
                     </a>
@@ -169,7 +180,7 @@
             <!-- Divider -->
             <hr class="my-3">
             <!-- Heading 
-            <h6 class="navbar-heading text-muted">Documentation</h6>--> 
+            <h6 class="navbar-heading text-muted">Documentation</h6>-->
             <!-- Navigation
             <ul class="navbar-nav mb-md-3">
                 <li class="nav-item">
