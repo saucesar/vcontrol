62ddb2a3da84ace85fb4b4f110cca0e6b98ec4f7
saucesar <sau.cesarlima2013@gmail.com>
2023-04-14 21:54:40 -0300

    Ajustes de validação

--- app/Http/Requests/Emails/StoreEmail.php
+++ app/Http/Requests/Emails/StoreEmail.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Emails;
 
+use App\Helpers\UserSession;
+use App\Rules\UniqueEmailRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEmail extends FormRequest
@@ -13,9 +15,10 @@ class StoreEmail extends FormRequest
 
     public function rules()
     {
+        $company = UserSession::getCompany();
         return [
-            'name' => 'required|min:5',
-            'email' => 'required|email:rfc|unique:emails',
+            'name' => ['required', 'min:5'],
+            'email' => ['required', 'email:rfc', new UniqueEmailRule($company->id)],
         ];
     }
 }
