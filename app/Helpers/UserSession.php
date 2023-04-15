ec55588f1a8ee3f2f8793ee4e949c4891c1a25ef
saucesar <sau.cesarlima2013@gmail.com>
2023-04-14 21:09:25 -0300

    Tipagem do retorno

--- app/Helpers/UserSession.php
+++ app/Helpers/UserSession.php
@@ -3,11 +3,11 @@
 namespace App\Helpers;
 
 use App\Http\Controllers\CompanyController;
-
+use App\Models\Company;
 
 class UserSession
 {
-    public static function getCompany()
+    public static function getCompany(): Company
     {
         return (new CompanyController())->getCompanyInSession();
     }
